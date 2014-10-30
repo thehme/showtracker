b2f3a805686a63a19f8095eaeef8b7d04682cb99
lei-clearsky <sportzhulei@gmail.com>
2014-10-29 22:39:12 -0400

    Completed step 7 and part of Step 9, tested everything working well

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
+var _ = require('lodash');
+
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var showSchema = new mongoose.Schema({
   _id: Number,
   name: String,
@@ -69,11 +78,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
 
 app.get('/api/shows', function(req, res, next) {
   var query = Show.find();
@@ -97,7 +107,124 @@ app.get('/api/shows/:id', function(req, res, next) {
   });
 });
 
+// route for adding a new TV show
+app.post('/api/shows', function(req, res, next) {
+  var apiKey = '9EF1D1E7D28FDA0B';
+  var parser = xml2js.Parser({
+    explicitArray: false,
+    normalizeTags: true
+  });
+  var seriesName = req.body.showName
+    .toLowerCase()
+    .replace(/ /g, '_')
+    .replace(/[^\w-]+/g, '');
+  
+  async.waterfall([
+    function(callback) {
+      request.get('http://thetvdb.com/api/GetSeries.php?seriesname=' + seriesName, function(error, response, body) {
+        if (error) return next(error);
+        parser.parseString(body, function(err, result) {
+          if (!result.data.series) {
+            return res.send(404, { message: req.body.showName + ' was not found.' });
+          }
+          var seriesId = result.data.series.seriesid || result.data.series[0].seriesid;
+          callback(err, seriesId);
+        });
+      });
+    },
+    function(seriesId, callback) {
+      request.get('http://thetvdb.com/api/' + apiKey + '/series/' + seriesId + '/all/en.xml', function(error, response, body) {
+        if (error) return next(error);
+        parser.parseString(body, function(err, result) {
+          var series = result.data.series;
+          var episodes = result.data.episode;
+          var show = new Show({
+            _id: series.id,
+            name: series.seriesname,
+            airsDayOfWeek: series.airs_dayofweek,
+            airsTime: series.airs_time,
+            firstAired: series.firstaired,
+            genre: series.genre.split('|').filter(Boolean),
+            network: series.network,
+            overview: series.overview,
+            rating: series.rating,
+            ratingCount: series.ratingcount,
+            runtime: series.runtime,
+            status: series.status,
+            poster: series.poster,
+            episodes: []
+          });
+          _.each(episodes, function(episode) {
+            show.episodes.push({
+              season: episode.seasonnumber,
+              episodeNumber: episode.episodenumber,
+              episodeName: episode.episodename,
+              firstAired: episode.firstaired,
+              overview: episode.overview
+            });
+          });
+          callback(err, show);
+        });
+      });
+    },
+    function(show, callback) {
+      var url = 'http://thetvdb.com/banners/' + show.poster;
+      request({ url: url, encoding: null }, function(error, response, body) {
+        show.poster = 'data:' + response.headers['content-type'] + ';base64,' + body.toString('base64');
+        callback(error, show);
+      });
+    }
+  ], function(err, show) {
+    if (err) return next(err);
+    show.save(function(err) {
+      if (err) {
+        if (err.code == 11000) {
+          return res.send(409, { message: show.name + ' already exists.' });
+        }
+        return next(err);
+      }
+      res.send(200);
+    });
+  });
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) next();
+  else res.send(401);
+}
+
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    show.subscribers.push(req.user.id);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+  Show.findById(req.body.showId, function(err, show) {
+    if (err) return next(err);
+    var index = show.subscribers.indexOf(req.user.id);
+    show.subscribers.splice(index, 1);
+    show.save(function(err) {
+      if (err) return next(err);
+      res.send(200);
+    });
+  });
+});
+// create redirect route before error handler
+app.get('*', function(req, res) {
+  res.redirect('/#' + req.originalUrl);
+});
+
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.send(500, { message: err.message });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });

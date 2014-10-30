cd56094b57a1c7698c2225aee1aaabafbb29e2c9
thehme <bcortez23@gmail.com>
2014-10-30 19:19:49 -0400

    changed Step 10 label to Step 11

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// Step 10
+// Step 11
 agenda.define('send email alert', function(job, done) {
   Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
     var emails = show.subscribers.map(function(user) {
